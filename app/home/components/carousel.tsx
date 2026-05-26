1103be186baf8f06276ae8cb66bf9dae72397585
nox <nox.heights@gmail.com>
2026-05-26 10:59:05 +0200

    fix: make carousel images 100% width relative to container

--- app/home/components/carousel.tsx
+++ app/home/components/carousel.tsx
@@ -19,13 +19,15 @@ export default function Carousel() {
   }, []);
 
   return (
-    <div className="mx-auto w-[262px] lg:w-[568px] overflow-hidden" data-testid="carousel">
+    <div className="mx-auto w-full overflow-hidden" data-testid="carousel">
       <div
-        className="flex gap-[23px] transition-transform duration-500 ease-in-out max-lg:-translate-x-[calc(var(--c)*285px)] lg:-translate-x-[calc(var(--c)*591px)]"
-        style={{ "--c": current } as React.CSSProperties}
+        className="flex gap-[23px] transition-transform duration-500 ease-in-out"
+        style={{
+          transform: `translateX(calc(${current} * -1 * (100% + 23px)))`,
+        }}
       >
         {slides.map((slide, i) => (
-          <div key={i} className="h-[175px] w-[262px] shrink-0 lg:h-[380px] lg:w-[568px]">
+          <div key={i} className="h-[175px] w-full shrink-0 lg:h-[380px]">
             <img src={slide.src} alt={slide.alt} className="h-full w-full object-cover rounded-[5px]" />
           </div>
         ))}
